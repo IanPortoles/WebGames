c1da230b4ff8e277ef34565ed0788e80cbfd6bbc
IanPortoles <iportolesvilalta@gmail.com>
2024-02-20 17:39:26 +0100

    Test git

---
+++ BackEnd/BackEnd/Model/TarjetaCredito.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+namespace BackEnd.Model
+{
+    public class TarjetaCredito
+    {
+        [Key]
+        public int Id {  get; set; }
+
+        [Required]
+        [Column(TypeName ="varchar(100)")]
+        public required string Titular { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(16)")]
+        public required string NumeroTarjeta { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+        public required string FechaExpiracion { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(3)")]
+        public required string CVV { get; set; }
+    }
+}
