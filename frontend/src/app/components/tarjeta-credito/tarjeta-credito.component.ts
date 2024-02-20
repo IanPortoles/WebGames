1eaeb9d9e32631731c4668704e09b72f57eb318f
IanPortoles <iportolesvilalta@gmail.com>
2024-02-20 21:21:33 +0100

    Login css

--- frontend/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ frontend/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-tarjeta-credito',
   templateUrl: './tarjeta-credito.component.html',
   styleUrls: ['./tarjeta-credito.component.css']
 })
-export class TarjetaCreditoComponent {
+export class TarjetaCreditoComponent implements OnInit {
+  listTarjetas:any[]=[
+    {titulo:'Juan Perez',numeroTarjeta:'223242424242', fechaExpiracaion:'11/12',cvv:'123'},
+    {titulo:'Miguel Perez',numeroTarjeta:'99999999999', fechaExpiracaion:'01/02',cvv:'567'}
+  ];
+  listUsuarios:any[]=[
+    {titulo:'ramon',contra:'123'},
+    {titulo:'carlos',contra:'456'}
+  ]
+  form: FormGroup;
 
+  
+  constructor(private fb:FormBuilder){
+    this.form=this.fb.group({
+      titulo:[''],
+      contra:['']
+    })
+    }
+  
+  ngOnInit(): void {
+    
+  }
+  agregarUsuario(){
+    const usuario:any={
+      titulo: this.form.get('titulo')?.value,
+      contra: this.form.get('contra')?.value
+
+    }
+    this.listUsuarios.push(usuario)
+    this.form.reset()
+    console.log(usuario)
+  }
 }
